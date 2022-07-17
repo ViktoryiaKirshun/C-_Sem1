7ecb3f7dcc108936d83bc7ab2a993382084dc36a
Viktoria <kirvikanik@gmail.com>
2022-07-17 13:52:16 +0300

    Ex 2,4,6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 2 
+//int a = 5;
+//int b = 7;
+//int max = a;
+//if(b > max) max = b;
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+// задача 4 
+//int a = 44;
+//int b = 5;
+//int c = 78;
+//int max = a;
+//if(b > max) max = b;
+//if(c > max) max = c;
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+//задача 6
+//int a;
+//Console.Write("введите число: ");
+//a = Console.Read();
+//if(a % 2 == 1) 
+//{
+ //   Console.WriteLine("нечетное");
+//}
+//else
+//{
+  //  Console.WriteLine("четное");
+//}
+//Console.ReadKey();
